aee30a1a386212aaee9fd2ffe579a2ed08600b1b
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-09 16:25:55 +0200

    Initial Commit

---
+++ Shared/PagerTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerTabsApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PagerTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
